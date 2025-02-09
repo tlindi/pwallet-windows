7d9b3ffe09bdb8820afdaae3021da3453a840f72
Hodladi <91490683+Hodladi@users.noreply.github.com>
2025-02-09 10:24:51 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,17 +1,12 @@
-using System.Collections.Concurrent;
-using Microsoft.AspNetCore.DataProtection;
 using pWallet.Components;
 using pWallet.Controllers;
-using pWallet.Interfaces;
 using pWallet.Services;
+using System.Collections.Concurrent;
+using pWallet.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.WebHost.UseUrls("http://0.0.0.0:3939");
-
-builder.Services.AddDataProtection()
-	.PersistKeysToFileSystem(new DirectoryInfo("/app-keys"))
-	.SetApplicationName("pWallet");
 
 builder.Services.AddRazorComponents()
 	.AddInteractiveServerComponents();
@@ -28,6 +23,7 @@ builder.Services.AddScoped<QrService>();
 builder.Services.AddSingleton(new ConcurrentDictionary<string, ConcurrentBag<SseController.SseClient>>());
 builder.Services.AddSingleton<IBitcoinPriceService, BitcoinPriceService>();
 
+
 builder.Services.AddControllers();
 
 var app = builder.Build();
@@ -35,7 +31,10 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
 	app.UseExceptionHandler("/Error", createScopeForErrors: true);
+	//app.UseHsts();
 }
+
+//app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
