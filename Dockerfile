f023ce7a06ba9f0857250427a42b2e7b0668e345
hodladi <hodladi@hodla.nu>
2025-02-09 10:10:25 +0100

    Dockerfile and persisted keys to Program.cs

--- Dockerfile
+++ Dockerfile
@@ -3,13 +3,14 @@ WORKDIR /app
 
 COPY . ./
 RUN dotnet restore
-
 RUN dotnet publish -c Release -o /out --no-restore
 
 FROM mcr.microsoft.com/dotnet/aspnet:8.0
 WORKDIR /app
 
 COPY --from=build /out /app
+
+VOLUME ["/app-keys"]
 
 EXPOSE 3939
 
--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
+using System.Collections.Concurrent;
+using Microsoft.AspNetCore.DataProtection;
 using pWallet.Components;
 using pWallet.Controllers;
-using pWallet.Services;
-using System.Collections.Concurrent;
 using pWallet.Interfaces;
+using pWallet.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.WebHost.UseUrls("http://0.0.0.0:3939");
+
+builder.Services.AddDataProtection()
+	.PersistKeysToFileSystem(new DirectoryInfo("/app-keys"))
+	.SetApplicationName("pWallet");
 
 builder.Services.AddRazorComponents()
 	.AddInteractiveServerComponents();
@@ -23,7 +28,6 @@ builder.Services.AddScoped<QrService>();
 builder.Services.AddSingleton(new ConcurrentDictionary<string, ConcurrentBag<SseController.SseClient>>());
 builder.Services.AddSingleton<IBitcoinPriceService, BitcoinPriceService>();
 
-
 builder.Services.AddControllers();
 
 var app = builder.Build();
@@ -31,10 +35,7 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
 	app.UseExceptionHandler("/Error", createScopeForErrors: true);
-	//app.UseHsts();
 }
-
-//app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
